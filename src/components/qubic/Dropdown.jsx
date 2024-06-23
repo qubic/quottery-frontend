557d16d670672fa9120be9e76bc345aea756acd2
couch42 <wanted@monoape.com>
2024-06-23 14:51:28 +0200

    Add basic dropdown component

---
+++ src/components/qubic/Dropdown.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+const Dropdown = ({ label, options, selected, setSelected }) => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen)
+    }
+
+    return (
+        <div className="relative">
+            <span
+                className="text-primary-40 font-space cursor-pointer"
+                onClick={toggleDropdown}
+            >
+                {label}
+            </span>
+        {isDropdownOpen && (
+            <div className="absolute mt-2 w-48 bg-white border border-gray-300 rounded shadow-lg">
+                {options.map((option, index) => (
+                    <div
+                        key={index}
+                        className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer"
+                        onClick={() => {
+                            setSelected(index)
+                            toggleDropdown()
+                        }}
+                    >
+                        {option.label}
+                    </div>
+                ))}                
+            </div>
+        )}
+        </div>
+    )
+}
+
+export default Dropdown
