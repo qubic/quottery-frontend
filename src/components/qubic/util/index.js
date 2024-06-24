dfa029fef9a4b34eab2c51857b4859622380bb8e
couch42 <wanted@monoape.com>
2024-06-24 14:00:08 +0200

    Add truncateMiddle string formater

--- src/components/qubic/util/index.js
+++ src/components/qubic/util/index.js
@@ -1,4 +1,12 @@
 // format number input to 100,000,000 format
 export const formatQubicAmount = (amount, seperator = ',') => {
-    return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, seperator);
+    return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, seperator)
+}
+
+export const truncateMiddle = (str, charsToRemove) => {
+    const length = str.length
+    const start = Math.floor((length - charsToRemove) / 2)
+    const end = start + charsToRemove
+  
+    return str.slice(0, start) + '...' + str.slice(end)
 }
