d45a9bc15f85e03ea716cfd51a687fe1e0295cf9
couch42 <wanted@monoape.com>
2024-06-24 14:00:49 +0200

    Use external LabelData component

--- src/components/BetOverviewCard.js
+++ src/components/BetOverviewCard.js
@@ -3,19 +3,9 @@ import Card from "./qubic/Card"
 import QubicCoin from "../assets/qubic-coin.svg"
 import { formatQubicAmount } from "./qubic/util"
 import MinMaxSpan from "./MinMaxSpan"
+import LabelData from "./LabelData"
 
 function BetOverviewCard({ data, onClick }) {
-
-  const LabelData = ({lbl, value}) => (
-    <div className="flex flex-col justify-center items-center">
-      <span className="text-gray-50 text-12">
-        {lbl}
-      </span>
-      <span className="text-white text-16">
-        {value}
-      </span>
-    </div>
-  )
 
   return (
     <Card 
