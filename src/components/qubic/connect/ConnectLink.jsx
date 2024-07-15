6e728fa3fc187406ef8078b31ff535f27d3f324c
couch42 <wanted@monoape.com>
2024-07-15 15:24:28 +0200

    Add pointer style to whole button

--- src/components/qubic/connect/ConnectLink.jsx
+++ src/components/qubic/connect/ConnectLink.jsx
@@ -4,30 +4,30 @@ import ConnectModal from "./ConnectModal"
 import { useQubicConnect } from "./QubicConnectContext"
 
 const ConnectLink = () => {
-    const { connected, showConnectModal, toggleConnectModal } = useQubicConnect()    
+  const { connected, showConnectModal, toggleConnectModal } = useQubicConnect()
 
-    return (<>
-        <div className="absolute right-12 sm:right-12 flex gap-[10px] justify-center items-center"
-            onClick={() => toggleConnectModal()}
-        >
-            {connected ? <>
-                <span className='hidden md:block font-space text-[16px] text-gray-50 mt-[5px] font-[500]'>
-                    Lock Wallet
-                </span>
-                <img src={lock} alt="locked lock" className=' cursor-pointer' />
-            </> : <>
-                <span className='hidden md:block font-space text-[16px] text-gray-50 mt-[5px] font-[500]'>
-                    Unlock Wallet
-                </span>
-                <img src={unlocked} alt="unlocked lock" className=' cursor-pointer' />
-            </>
-            }
-        </div>
-        <ConnectModal 
-            open={showConnectModal}
-            onClose={() => toggleConnectModal()}
-        />
-    </>)
+  return (<>
+    <div className="absolute right-12 sm:right-12 flex gap-[10px] justify-center items-center cursor-pointer"
+      onClick={() => toggleConnectModal()}
+    >
+      {connected ? <>
+        <span className='hidden md:block font-space text-[16px] text-gray-50 mt-[5px] font-[500]'>
+          Lock Wallet
+        </span>
+        <img src={lock} alt="locked lock icon" />
+      </> : <>
+        <span className='hidden md:block font-space text-[16px] text-gray-50 mt-[5px] font-[500]'>
+          Unlock Wallet
+        </span>
+        <img src={unlocked} alt="unlocked lock icon" />
+      </>
+      }
+    </div>
+    <ConnectModal
+      open={showConnectModal}
+      onClose={() => toggleConnectModal()}
+    />
+  </>)
 }
 
 export default ConnectLink
